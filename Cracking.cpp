f1f61302742a59e0409c53a89fc57f71c9b4d250
Lam T Duong <lamthachduong@gmail.com>
2021-08-14 22:47:14 -0400

    Deleted question 3.1. Waste of time.

--- Cracking.cpp
+++ Cracking.cpp
@@ -832,21 +832,18 @@ public:
         }
     };
 
-    /*
-    ** 3.1) Three in One:
-    ** ----------------------------------------------------------------------
-    ** Describe how you could use a single array to implement three stacks.
-    */
 
+    /*
+    ** 3.2) Stack Min:
+    ** ----------------------------------------------------------------------
+    ** How would you design a stack which, in addition to push and pop, has a
+    ** function min which returns the minimum element? Push, pop, and min should
+    ** all operate in O(1) time.
+    */
 
 };
 
 int main()
 {
-    std::vector<int> a = {5, 4, 3, 2, 1};
-    Cracking::Node list1(a);
-    auto list = &list1;
-    Cracking::mergeSortLinkedList(&list);
-    list->printList();
     return 0;
 }
