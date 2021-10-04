b38da0b20878f862a864b018e476c13893ec07eb
Lam T Duong <lamthachduong@gmail.com>
2021-10-04 10:28:10 -0400

    New unit testing file.

---
+++ UnitTests.cpp
@@ -0,0 +1,17 @@
+#include "DataStructures.hpp"
+#define BOOST_TEST_MODULE CrackingTest
+#include <boost/test/unit_test.hpp>
+
+using namespace DataStructures;
+
+BOOST_AUTO_TEST_CASE(TreeTest)
+{
+    BinaryTree bt;
+    BOOST_CHECK(bt.validTree());
+    bt.insert(1);
+    bt.insert(2);
+    bt.insert(3);
+    bt.insert(4);
+    bt.insert(5);
+    bt.insert(6);
+}
